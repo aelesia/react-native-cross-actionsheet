8354d0593f735090ce8cf869b239a787ab4e8f15
Jaslyn S <7008316+aelesia@users.noreply.github.com>
2020-06-07 23:22:34 +0800

    Fixed package.json, npmignore ESLint/Prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,11 +2,11 @@ module.exports = {
   root: true,
   extends: '@react-native-community',
   rules: {
-    "prettier/prettier": "warn",
-    "no-console":["warn", { allow: ["warn", "error"] }],
-    "semi": "off", // Handled by prettierrc.js
-    "comma-dangle": "off", // Handled by prettierrc.js
-    "react-native/no-inline-styles": "off",
-    "@typescript-eslint/no-unused-vars":["warn", { "argsIgnorePattern": "^_" }]
+    'prettier/prettier': 'warn',
+    'no-console': ['warn', { allow: ['warn', 'error'] }],
+    semi: 'off', // Handled by prettierrc.js
+    'comma-dangle': 'off', // Handled by prettierrc.js
+    'react-native/no-inline-styles': 'off',
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }]
   }
-};
+}
