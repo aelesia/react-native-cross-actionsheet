b10a0863dc0d2e48c8f43292e13f4d18793ce413
cezzzanne <pablo.arellanos97@gmail.com>
2021-12-16 14:06:26 -0500

    fix linter issues

--- index.ts
+++ index.ts
@@ -1,3 +1,3 @@
-export { ActionSheet, ActionSheetConfig, ActionSheetOption } from './src/ActionSheet'
-export { ActionSheetAndroid, ActionSheetAndroidOptions } from './src/ActionSheetAndroid'
-export { ActionSheetCancelledError } from './src/ActionSheetCancelledError'
+export * from './src/ActionSheet'
+export * from './src/ActionSheetAndroid'
+export * from './src/ActionSheetCancelledError'
